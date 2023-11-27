2ac87cf46c2d76c62c3ebf3c78ad5be9cd346bce
Denys <denis.melnyk.2006@gmail.com>
2023-11-27 16:39:43 +0100

    aga

--- mysite/calculator/urls.py
+++ mysite/calculator/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
-from .views import hello
+from .views import hello, calc, get_users, add_user, login
 
 
 urlpatterns = [
-    path('hello/<int:number>', hello)
+    path('hello/<int:number>', hello),
+    path('calc', calc),
+    path('users', get_users),
+    path('add_user', add_user),
+    path('login', login)
+    
+    
 ]
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['192.168.1.40','alt.gameoneyt.com']
 # Application definition
 
 INSTALLED_APPS = [
+    'calculator.apps.CalculatorConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
